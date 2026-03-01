da02af208d3a07d34309736aea32e2dbc1c73b4d
Gergo Mahunka <gmahunka@gmail.com>
2026-03-01 17:41:58 +0100

    Initial Commit

---
+++ CardGameByMahu/CardGameByMahuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameByMahuApp.swift
+//  CardGameByMahu
+//
+//  Created by Gergo Mahunka on 2026. 03. 01..
+//
+
+import SwiftUI
+
+@main
+struct CardGameByMahuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
